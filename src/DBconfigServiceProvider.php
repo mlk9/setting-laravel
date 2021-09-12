ee3b224716c9be7561e7c083713b5496a7599bc6
Mohammad Maleki <malekii24@outlook.com>
2021-09-12 07:31:30 +0430

    composer init & start

---
+++ src/DBconfigServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Mlk9\DBconfig;
+
+use Illuminate\Support\ServiceProvider;
+
+class DBconfigServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+
+    /**
+     * Boot any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+
+}
