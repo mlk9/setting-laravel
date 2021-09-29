40a881e0432f6ed15b4f6ebc86c3f39aa30b867c
Mohammad Maleki <malekii24@outlook.com>
2021-09-29 11:06:58 +0330

    change set method

--- src/DBConfig.php
+++ src/DBConfig.php
@@ -21,7 +21,7 @@ class DBConfig
      * @param mixed $value
      * @return void
      */
-    public function set($key,$value)
+    private function setNone($key,$value)
     {
         $valueEncrypt =  Crypt::encryptString($value);
         if (is_null(DB::table($this->table)->where('key',$key)->get()->first())) {
@@ -30,6 +30,38 @@ class DBConfig
             DB::table($this->table)->where('key',$key)->update(['value'=>$valueEncrypt]);
         }
     }
+
+    /**
+     * set configure
+     *
+     * @param array $configs
+     * @return void
+     */
+    private function setArray($array)
+    {
+        foreach ($array as $key => $value) {
+            $valueEncrypt =  Crypt::encryptString($value);
+            if (is_null(DB::table($this->table)->where('key',$key)->get()->first())) {
+                DB::table($this->table)->insert(['key'=>$key,'value'=>$valueEncrypt]);
+            }else{
+                DB::table($this->table)->where('key',$key)->update(['value'=>$valueEncrypt]);
+            }
+        }
+    }
+
+    public function __Call($method,$arguments)
+    {
+        if($method=='set')
+        {
+            if (count($arguments) == 2) {
+                return call_user_func_array(array($this,'setNone'), $arguments);
+            }   
+            if (count($arguments) == 1) {
+                return call_user_func_array(array($this,'setArray'), $arguments);
+            }
+        }
+    }
+
 
     /**
      * get configure
